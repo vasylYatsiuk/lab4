38e959fa7df245f88ffc8f1a6e0d6ae2b7ae12e4
Vasyl Yatsiuk <vasyl.yatsiuk-ki211@nung.edu.ua>
2022-06-20 22:42:02 +0300

    first

---
+++ ajax.js
@@ -0,0 +1,59 @@
+$(document).ready(function () {
+    $.get("https://my-json-server.typicode.com/vladislavkovtun2003/AJAX/posts", function (data) {
+        console.log(data);
+        for (i = 0; i < data.length; i++) {
+            $("table tbody").append(`
+            <tr>
+              <th>`+ data[i].lastElementNumber + `</th>
+              <th>`+ data[i].nameInput + `</th>
+              <th>`+ data[i].author + `</th>
+              <th>`+ data[i].ganre + `</th>
+              <th>`+ data[i].date + `</th>
+              <th>`+ data[i].sellCount + `</th>
+              <th>`+ data[i].price + `</th>
+              <th>`+ data[i].dateSell + `</th>
+              <th>`+ data[i].receivedCount + `</th>
+              <th>`+ data[i].image + `</th>
+              <th><a class="delete" href="#" data-id="`+ data[i].id + `">Delete</a>
+            </tr>
+            `);
+        }
+    });
+});
+$(document).on("click", ".delete", function (e) {
+    e.preventDefault();
+    id = $(this).data("id");
+    $.post("https://my-json-server.typicode.com/vladislavkovtun2003/AJAX/posts", { action: "delete", id: id }, function (data) {
+        console.log(data);
+    }
+    );
+});
+$(document).ready(function () {
+    $("#inputBtn").bind("click", function () {
+        $.ajax({
+            method: "POST",
+            url: "https://my-json-server.typicode.com/vladislavkovtun2003/AJAX/posts",
+            data: {
+                nameInput: $("#nameInput").val(),
+                author: $("#author").val(),
+                ganre: $("#ganre").val(),
+                date: $("date").val(),
+                sellCount: $("#sellCount").val(),
+                price: $("#price").val(),
+                dateSell: $("#dateSell").val(),
+                receivedCount: $("#receivedCount").val(),
+                image: $("#image").val(),
+            },
+            dataType: "html", beforeSend: function () {
+                $("loading").show();
+            },
+            success: function (data) {
+                if (data == "Success") {
+                    // ... 
+                }
+            }
+        });
+    });
+});
+
+
---
+++ script.js
@@ -0,0 +1,29 @@
+$(document).ready(
+    function () {
+        $(document).on("submit", "#inputForm", function (e) {
+            e.preventDefault();
+            var lastElementNumber = Number($('table tbody tr:last td:first').text());
+            $("table tbody").append(`<tr data-index="`+(lastElementNumber+1)+`">
+                                    <td>`+(lastElementNumber+1)+`</td>
+                                    <td>`+$("#nameInput").val()+`</td>
+                                    <td>`+ $("#author").val()+`</td>
+                                    <td>`+ $("#ganre").val()+`</td>
+                                    <td>`+$("#date").val()+`</td>
+                                    <td>`+ $("#sellCount").val()+`</td>
+                                    <td>`+ $("#price").val()+`</td>
+                                    <td>`+ $("#dateSell").val()+`</td>
+                                    <td>`+ $("#receivedCount").val() +`</td>
+                                    <td><img src="`+ $(" #image").val() +`" width="80px" height="100px"></td>
+                                    <td><a data-index="`+ (lastElementNumber + 1) +`" class="delete" href="#">Delete</a></td>
+                                </tr>`);
+            $("#inputForm")[0].reset();
+        });
+        $(document).on("click",".delete", function(e){
+            e.preventDefault();
+            let deleteNum=$(this).parent().parent().children().first().text();
+            $(this).parent().parent().nextAll().each(function (index) {
+                $(this).children().first().text(Number($(this).children().first().text())-1);
+            });
+            $(this).parent().parent().remove();
+        });
+    });
